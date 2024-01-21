fb38b58273a5cdfd55c4707efc31999fd04b8520
Suteejankhaw <119989200+Suteejankhaw@users.noreply.github.com>
2024-01-21 12:56:07 +0700

    Update bills.service.ts

--- backend/src/bills/bills.service.ts
+++ backend/src/bills/bills.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BillsService {}
+export class BillsService {
+  private bills: Bill[] = [];
+
+  createBill(billData: Bill): void {
+    this.bills.push(billData);
+  }
+
+  getAllBills(): Bill[] {
+    return this.bills;
+  }
+
+  getBillById(billId: string): Bill | undefined {
+    return this.bills.find((bill) => bill.id === billId);
+  }
+
+  updateBill(billId: string, updatedData: Partial<Bill>): Bill | undefined {
+    const billIndex = this.bills.findIndex((bill) => bill.id === billId);
+
+    if (billIndex !== -1) {
+      this.bills[billIndex] = { ...this.bills[billIndex], ...updatedData };
+      return this.bills[billIndex];
+    }
+
+    return undefined;
+  }
+
+  deleteBill(billId: string): void {
+    this.bills = this.bills.filter((bill) => bill.id !== billId);
+  }
+}
+
+interface Bill {
+  id: string;
+  amount: number;
+  description: string;
+  // Add more properties as needed
+}
