0479b84e49d8409ded38ff7f9ef6d3f1d5a3e9fd
aoomUwU <65022645@up.ac.th>
2024-02-07 19:43:57 +0700

    Update index.vue

--- frontend/pages/A1_testAPI/allHome/index.vue
+++ frontend/pages/A1_testAPI/allHome/index.vue
@@ -1,7 +1,32 @@
 <template>
     <div>
+        <p>Homes</p>
+        <div class="allHome"></div>
     </div>
 </template>
 
 <script setup lang="ts">
+    import { ref, computed } from 'vue'
+    import { useRoute, useRouter } from 'vue-router'
+
+    
+    const runtimeConfig = useRuntimeConfig()
+    let BASE_URL = runtimeConfig.public.BASE_URL
+
+
+    const lands = await $fetch(BASE_URL + `/lands`, {
+      method: 'GET',
+    })
+    setTimeout(() => {
+      const allHomeContainer = document.querySelector('.allHome')
+      lands.forEach(land => {
+        const home = document.createElement('p')
+        home.textContent = `HomeID: ${land.id}`
+        allHomeContainer.appendChild(home)
+
+        const saveBill = document.createElement('button')
+        saveBill.textContent = `saveBill`
+        allHomeContainer.appendChild(saveBill)
+      });
+    });
 </script>
