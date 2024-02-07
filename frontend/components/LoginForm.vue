08cfb3f2a2802dbd9b20de69c377560f8b42397c
aoomUwU <65022645@up.ac.th>
2024-02-07 21:59:13 +0700

    login

--- frontend/components/LoginForm.vue
+++ frontend/components/LoginForm.vue
@@ -9,11 +9,12 @@
     
     <p class="mt-2 text-sm text-indigo-500">โปรดกรอกข้อมูลเพื่อนลงชื่อเข้าใช้</p>
     <p class="mt-5 text-sm font-semibold text-gray-500">หมายเลขบัตรประชาชน</p>
-    <input class="mt-1 rounded border py-1 px-3 text-sm shadow" />
+    <input class="mt-1 rounded border py-1 px-3 text-sm shadow" v-model="userId"/>
     <p class="mt-5 text-sm font-semibold text-gray-500">เบอร์โทรศัพท์</p>
     <input
       class="mt-1 rounded border py-1 px-3 text-sm text-sm shadow"
       type="password"
+      v-model="phone_no"
     />
     <div class="mt-5 flex items-center">
       <input class="mr-2 align-middle text-sm text-sm" type="checkbox" />
@@ -27,8 +28,53 @@
     </div>
     <button
       class="mt-5 rounded border bg-indigo-400 py-2 px-5 text-sm text-sm font-semibold text-gray-50 shadow hover:bg-indigo-500"
+      @click="login()"
     >
       ลงชื่อเข้าใช้
     </button>
   </div>
 </template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import { useRouter } from 'vue-router'
+
+  const runtimeConfig = useRuntimeConfig()
+  let BASE_URL = runtimeConfig.public.BASE_URL
+  let userId = ''
+  let phone_no = ''
+  let loginStatus = ref('')
+
+  const router = useRouter()
+  const goToUser = (userId) => {
+      router.push(`/userInfo/${userId}`)
+  }
+  const goToCollector = (userId) => {
+      router.push(`/A1_testAPI/collect/${userId}`)
+  }
+  const goToAdmin = (userId) => {
+      router.push(`/A1_testAPI/admin/${userId}`)
+  }
+
+  async function login() {
+    let ENDPOINT = `/users/${userId}`
+    const users = await $fetch(BASE_URL + ENDPOINT, {
+      method: 'GET',
+    })
+    if (phone_no == users.phone_no) {
+      if (users.role == 'Admin') {
+        console.log('Admin')
+        goToAdmin(users.id)
+      } else if (users.role == 'Collector') {
+        console.log('Collector')
+        goToCollector(users.id)
+      } else if (users.role == 'User') {
+        console.log('User')
+        goToUser(users.id)
+      }
+    } else {
+      loginStatus.value = 'invalid'
+      console.log('invalid id or number')
+    }
+  }
+</script>
---
+++ frontend/pages/login/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <div class="flex">
+        <div class="min-h-screen flex-1">
+          <div class="flex h-full items-center py-8 px-5">
+            <LoginForm />
+          </div>
+        </div>
+        <div class="hidden min-h-screen flex-1 bg-gray-60 px-5 py-5 lg:flex">
+          <div class="mx-auto flex h-full items-center">
+            <img src="assets/images/logo.jpg" width="450" height="450">
+            <NuxtLogo />
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
