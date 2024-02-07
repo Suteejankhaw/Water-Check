23b46153b63d7c5769ea5e244ffdc6265ad0a584
aoomUwU <65022645@up.ac.th>
2024-02-07 21:20:50 +0700

    ={

---
+++ frontend/pages/login/login.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <div class="flex">
+        <div class="min-h-screen flex-1">
+          <div class="flex h-full items-center py-8 px-5">
+            <LoginForm />
+          </div>
+        </div>
+        <div class="hidden min-h-screen flex-1 bg-gray-60 px-5 py-5 lg:flex">
+          <div class="mx-auto flex h-full items-center">
+            <img src="assets/images/logo.jpg" width="450" height="450">
+            <NuxtLogo />
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  export default {
+    name: 'LoginPage',
+    layout: 'login',
+    methods :{
+        async fetchData() {
+      try {
+        const response = await axios.get('https://example.com/api/data');
+        console.log(response.data); // ตัวอย่างการแสดงผลข้อมูลที่ได้จาก API
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+  },
+  mounted() {
+    // เรียกใช้งาน fetchData เด่อ
+    this.fetchData();
+  }
+}
+
+  </script>
